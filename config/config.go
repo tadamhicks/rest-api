fee9bfa2fba362ea24680b3e79b308ee7db5f887
Adam Hicks <thomas.adam.hicks@gmail.com>
2019-03-15 16:22:04 -0600

    env vars instead of toml

--- config/config.go
+++ config/config.go
@@ -2,18 +2,38 @@ package config
 
 import (
 	"log"
-	"github.com/BurntSushi/toml"
+//	"github.com/BurntSushi/toml"
+	"github.com/caarlos0/env"
 )
 
+type Config struct {
+	Server string `env:"RESTING_DB_IP"`
+	Database string `env:"RESTING_DB"`
+	Username string `env:"RESTING_DB_USER"`
+	Password string `env:"RESTING_DB_PASSWORD"`
+}
+
+
+func (c *Config) Read() {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("Failed to parse ENV")
+    }
+}
+
+/*
 type Config struct {
 	Server string
 	Database string
 	Username string
 	Password string
 }
+*/
 
+/*
 func (c *Config) Read() {
 	if _, err := toml.DecodeFile("config.toml", &c); err != nil {
 		log.Fatal(err)
 	}
 }
+*/
